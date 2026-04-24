7ddd0efbdb561f16597266b4ba227a1292d41530
Luis Fernandez <lfernnar@emeal.nttdata.com>
2026-04-24 12:14:34 +0200

    Initial Commit

---
+++ formulario/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  formulario
+//
+//  Created by Luis Fernandez Rodriguez on 24/04/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
